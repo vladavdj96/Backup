4da3e6cd5a132b2de7709a22883ecb6740312c41
Vladimir Djokic <vladavdj96@gmail.com>
2021-04-28 18:12:12 +0200

    Napravljene funkcije

--- Backup/backup_svih_fajlova_u_folderu.py
+++ Backup/backup_svih_fajlova_u_folderu.py
@@ -4,6 +4,7 @@ import send2trash
 
 os.chdir('C:\\Users\\vlada\\Documents\\Backup')
 
+
 def backup():
     fajlovi_u_folderu = os.listdir('.')
 
@@ -25,11 +26,18 @@ def backup():
         zipfajl.write(fajl, compress_type=zipfile.ZIP_DEFLATED)
         zipfajl.close()
 
-        # Pravi se lista zipovanih fajlova
-        lista = open('listafajlova.txt', 'a' )
-        lista.write(str(fajl) + '\n')
-
-        # Fajl se salje u trashcan
-        send2trash.send2trash(fajl)
+        formiranje_liste(fajl)
+        brisanje(fajl)
 
 
+def formiranje_liste(fajl):
+    # Pravi se lista zipovanih fajlova
+    lista = open('listafajlova.txt', 'a')
+    lista.write(str(fajl) + '\n')
+
+
+def brisanje(fajl):
+    # Fajl se salje u trashcan
+    send2trash.send2trash(fajl)
+
+backup()
